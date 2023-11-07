eed9c0a3bcd426833d287888080a18c79db76ed8
atorres97 <agstorres97@gmail.com>
2023-11-06 21:21:30 -0300

    actualizacion de JS

--- js/script.js
+++ js/script.js
@@ -1,85 +1,76 @@
 //Valor del ticket
 const valorTicket = 200;
 
-//Categorias de descuentos
 let descuentoEstudiante = 80;
 let descuentoTrainee = 50;
 let descuentoJunior = 15;
 
-// Variables de los elementos del formulario
 let nombre = document.getElementById("nombre");
-let divErrorNombre = document.getElementById("mensajeErrorNombre");
+let divErrorNombre = document.getElementById("errorNombre");
 let apellido = document.getElementById("apellido");
-let divErrorApellido = document.getElementById("mensajeErrorApellido");
+let divErrorApellido = document.getElementById("errorApellido");
 let mail = document.getElementById("mail");
-let divErrorMail = document.getElementById("mensajeErrorMail");
-let cantidadTickets = document.getElementById("cantidadTickets");
-let mensajeErrorCantTickets = document.getElementById("mensajeErrorCantTickets");
-let categoria = document.getElementById("categoriaSelect");
-let mensajeErrorCategoria = document.getElementById("mensajeErrorCategoria");
+let divErrorMail = document.getElementById("errorMail");
+let cantidadTkts = document.getElementById("cantidadTkts");
+let errorCantTkts = document.getElementById("errorCantTkts");
+let categoria = document.getElementById("categoria");
+let errorCategoria = document.getElementById("errorCategoria");
 
-// Función quitar todos los estilos de error en los campos que los tuvieran.
 const quitarClaseError = () => {
     let listaNodos = document.querySelectorAll(".form-control, .form-select");
-    for (let i = 0; i < listaNodos.length; i++) {
+    for (let i = 0; i < listaNodos.length; i++){
         listaNodos[i].classList.remove('is-invalid');
     }
     let listaNodosdiv = document.querySelectorAll(".invalid-feedback");
-    for (let i = 0; i < listaNodosdiv.length; i++) {
+    for (let i = 0; i < listaNodosdiv.length; i++){
         listaNodosdiv[i].classList.remove('mostrarError');
     }
 }
-//Calculo total a pagar
 const totalAPagar = () => { 
         quitarClaseError();
-        // Valida campos en Nombre, Apellido y Correo, si no hay datos correctos se aplica un estilo de error, hace foco en el campo en cuestion y se detiene la funcion.
-        if (nombre.value === "") {
+        if (nombre.value === ""){
             nombre.classList.add("is-invalid");
             divErrorNombre.classList.add("mostrarError");
             nombre.focus();
             return;
         }
-        if (apellido.value === "" ) {
+        if (apellido.value === ""){
             apellido.classList.add("is-invalid");
             divErrorApellido.classList.add("mostrarError");
             apellido.focus();
             return;
         }
-        if (mail.value === "") {
+        if (mail.value === ""){
             mail.classList.add("is-invalid");
             divErrorMail.classList.add("mostrarError");
             mail.focus();
             return;
         }
-        // Funcion para verificar si el e-mail es valido (no valida dominios)
         const emailValido = mail => {
             return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
         }
-        if (!emailValido(mail.value)) {
+        if (!emailValido(mail.value)){
             mail.classList.add("is-invalid");
             divErrorMail.classList.add("mostrarError");
             mail.focus();
             return;
         }
-        // Valida la seleccion de por lo menos 1 tkt, si no hay datos correctos se aplica un estilo de error, hace foco en el campo y se detiene la funcion.
-        if ( (cantidadTickets.value == 0) || (isNaN(cantidadTickets.value)) ) {
-            cantidadTickets.classList.add("is-invalid");
-            mensajeErrorCantTickets.classList.add("mostrarError");
-            cantidadTickets.focus();
+        if ((cantidadTkts.value == 0) || (isNaN(cantidadTkts.value))){
+            cantidadTkts.classList.add("is-invalid");
+            errorCantTkts.classList.add("mostrarError");
+            cantidadTkts.focus();
             return;
         }
-        // Valida la seleccion de una categoría, si no hay datos correctos se aplica un estilo de error, hace foco en el campo y se detiene la funcion.
-        if (categoria.value == "") {
+        if (categoria.value == ""){
             categoria.classList.add("is-invalid");
-            mensajeErrorCategoria.classList.add("mostrarError");
+            errorCategoria.classList.add("mostrarError");
             categoria.focus();
             return;
         }
-        //
-        let totalValorTickets = (cantidadTickets.value) * valorTicket;
-        switch (categoria.value) {
+        let totalValorTickets = (cantidadTkts.value) * valorTicket;
+        switch (categoria.value){
             case "0":
-                totalValorTickets = totalValorTickets ;
+                totalValorTickets = totalValorTickets;
                 break;
             case "1":
                 totalValorTickets = totalValorTickets - (descuentoEstudiante / 100 * totalValorTickets);
@@ -91,11 +82,9 @@ const totalAPagar = () => {
                 totalValorTickets = totalValorTickets - (descuentoJunior / 100 * totalValorTickets);
                 break;
         }
-        // Inserta el valor en el HTML
         totalPago.innerHTML = totalValorTickets;
     }
     botonResumen.addEventListener('click', totalAPagar);
-    // Función para el botón Borrar para que borre el valor
     const resetTotalAPagar = () => {
         quitarClaseError();
         totalPago.innerHTML = "";
